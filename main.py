7bab4479888f5a27a82057c56543622336815d1c
Tai Tran <107969594+MobaEnder@users.noreply.github.com>
2025-04-20 19:14:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import asyncio
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 @bot.event
 async def on_ready():
