7ec135965eda80e2ad56861a7f551f51107ccf8a
Tai Tran <107969594+MobaEnder@users.noreply.github.com>
2025-04-21 02:44:16 +0700

    Update transfer.py

--- cogs/transfer.py
+++ cogs/transfer.py
@@ -1,7 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from data_manager import get_balance, update_balance
+from data.data_manager import get_balance, update_balance
 
 
 class Transfer(commands.Cog):
