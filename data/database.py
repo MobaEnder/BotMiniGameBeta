caee9f887a75a4fc5164805adefcfdfc529dead0
Tai Tran <107969594+MobaEnder@users.noreply.github.com>
2025-04-21 01:29:32 +0700

    Update database.py

--- data/database.py
+++ data/database.py
@@ -1,4 +1,3 @@
-# data/database.py
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
@@ -21,6 +20,15 @@ def get_user(user_id):
             "xp": 0,
             "level": 1,
             "quote": "",
+            "job": None,
+            "last_daily": "1970-01-01T00:00:00",
+            "last_work": "1970-01-01T00:00:00",
+            "last_fish": "1970-01-01T00:00:00",
+            "last_mine": "1970-01-01T00:00:00",
+            "last_slot": "1970-01-01T00:00:00",
+            "last_lode": "1970-01-01T00:00:00",
+            "last_snake": "1970-01-01T00:00:00",
+            "last_bomb": "1970-01-01T00:00:00"
         }
         users_collection.insert_one(user)
     return user
